7602f43759f5b5e95602b2a6100ecc3d430f23c0
gaajj <daantas224@gmail.com>
2025-05-03 11:54:19 +0200

    Add Presence: Discord RPC

---
+++ .config/nvim/lua/gaaj/plugins/presence.lua
@@ -0,0 +1,21 @@
+return {
+  "andweeb/presence.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("presence").setup({
+      -- General options
+      auto_update         = true,
+      neovim_image_text   = "The One True Editor",
+      main_image          = "neovim", -- "neovim" or "file"
+
+      -- Rich Presence text options
+      editing_text        = "Editing %s",
+      file_explorer_text = "Browsing %s",
+      git_commit_text     = "Committing changes",
+      plugin_manager_text = "Managing plugins",
+      reading_text        = "Reading %s",
+      workspace_text      = "Working on %s",
+      line_number_text    = "Line %s out of %s",
+    })
+  end,
+}
