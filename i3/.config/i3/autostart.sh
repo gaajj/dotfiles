f421c4175f89fdd75420935bd86e7a9c586fb906
gaajj <daan@gaaj.dev>
2025-07-22 18:46:37 +0200

    Add i3 config

---
+++ i3/.config/i3/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Window rules
+
+# Monitor 1
+i3-msg "workspace 1; exec vesktop"
+# i3-msg "workspace 2; exec spotify-launcher"
+# i3-msg "workspace 3; exec steam"
+# Monitor 2
+# Monitor 3
+i3-msg "workspace 11; exec librewolf"
