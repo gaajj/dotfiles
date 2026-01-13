b809dd1cb36b1b5fe60e630f7a2fc6bf0c411a23
gaajj <daan@gaaj.dev>
2026-01-13 22:48:10 +0100

    chore(nvim): Swap mini.surround for nvim-surround
    Fixes bind on ys taking 's'y -> 's'S

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,21 +1,10 @@
 return {
 	{
-		'echasnovski/mini.surround',
+		'kylechui/nvim-surround',
+		version = '*',
 		event = 'VeryLazy',
-		opts = {
-			mappings = {
-				add = 'ys',
-				delete = 'ds',
-				replace = 'cs',
-
-				find = '',
-				find_left = '',
-				highlight = '',
-				update_n_lines = '',
-				suffix_last = '',
-				suffix_next = '',
-			},
-			search_method = 'cover_or_next',
-		},
+		config = function()
+			require('nvim-surround').setup({})
+		end,
 	},
 }
