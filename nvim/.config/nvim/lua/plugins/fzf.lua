b10fadf8985aa69279fc654cb661a8925253e986
gaajj <daan@gaaj.dev>
2026-03-06 22:22:01 +0100

    chore(nvim): make fzf default on vim select ui

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -3,6 +3,12 @@ return {
 		'ibhagwan/fzf-lua',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 
+		config = function(_, opts)
+			local fzf = require('fzf-lua')
+			fzf.setup(opts)
+			fzf.register_ui_select()
+		end,
+
 		opts = {
 			winopts = {
 				height = 0.60,
