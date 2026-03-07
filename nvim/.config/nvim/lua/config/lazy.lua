282c4272705a4e8056a2c2940e9ac52160a62255
gaajj <daan@gaaj.dev>
2026-03-07 23:15:31 +0100

    fix(nvim): disable plugin notification on startup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -16,7 +16,7 @@ require('lazy').setup({
 		{ import = 'plugins' },
 	},
 	defaults = { lazy = true },
-	checker = { enabled = true },
+	checker = { enabled = false },
 	performance = {
 		rtp = {
 			disabled_plugins = {
